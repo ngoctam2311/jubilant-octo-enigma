46ef95b5c30649b5e7b24f44f10493a9b6804372
ngoctam2311 <dtam6408@gmail.com>
2023-02-14 00:50:58 +0700

    oke

---
+++ thuon/config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Ảnh Này Đẹp Nha';
+const giftUrl = 'https://ngoctam2311.github.io/yeuthuon/';
+const eventName = 'Chúc Ngày 14-2 Của Ta';
+const titleCard = 'Tặng Biệt Thự';
+const contentCard = 'Chúc Ta Đã Có Một Năm Vui Vẻ, Hơi Chọc Em Nhiều Nhưng Mong Webside Này Chuộc Lỗi Được!! ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'emiu.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jng;base64, " + base64;
